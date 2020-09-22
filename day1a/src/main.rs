f1c386fe5b55931aa1c19a3246d15a96a1db7759
Rob Szumlakowski <1071175+rszumlakowski@users.noreply.github.com>
2020-09-22 10:35:26 -0400

    day1a complete

--- day1a/src/main.rs
+++ day1a/src/main.rs
@@ -1,9 +1,20 @@
+use std::env;
+use std::fs;
+
 fn fuel(mass: i64) -> i64 {
-    2
+    (mass / 3) - 2
+}
+
+fn calc(mass_str: &str) -> i64 {
+    fuel(mass_str.parse::<i64>().unwrap())
 }
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let input_filename = args.get(1).expect("No input filename provided");
+    let contents = fs::read_to_string(input_filename).expect("Could not read input file");
+    let fuel_sum: i64 = contents.lines().map(|line| calc(line)).sum();
+    println!("{}", fuel_sum);
 }
 
 #[cfg(test)]
